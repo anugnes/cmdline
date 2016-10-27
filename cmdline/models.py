b235a54633f95914bb6e8d58ab169b899099b64d
Alberto Nugnes <alberto.nugnes@first-utility.com>
2016-10-27 17:08:19 +0100

    Add server and user models

--- cmdline/models.py
+++ cmdline/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Server(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    host = models.CharField("Hostname", max_length=50)
+    is_live = models.BooleanField("Live server")
+    ssh_key = models.TextField("SSH key", max_length=512)
+
+
+class User(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
+    is_root = models.BooleanField()
+    is_sudo = models.BooleanField()
+
